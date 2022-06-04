8fb7347e10acb7599e2dc172000adafcb7cc049f
okafordizzy <okaforchiedozie69@gmail.com>
2022-06-03 17:43:44 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints all possible digit calcuations of different digits
+ * in ascending order, seperated by a comma followed by space
  *
- * Return: Always 0 (Success/completed)
+ * Return Always O.
  */
+
 int main(void)
-
 {
-int number_left;
-int number_right;
+	int digit1, digit2, digit3;
 
+	for (digit1 = 0; digit1 < 8; digit1++)
+	{
+	for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+	{
+	for (digit3 = digit2 + 1; digit3 < 10; digit3+++)
+	{
+		putchar((digit1 % 10) + '0');
+		putchar((digit2 % 10) + '0');
+		putchar((digit3 % 10) + '0');
 
-for (number_left = 48; number_right <= 78; number_left++)
-{
-for (number_right = number_left + 1 ; number_right <= 78; number_right++)
-{
+		if (digit1 ==7 && digit2 == 8 && digit3 ==9)
 
-putchar(number_left);
-putchar (number_right);
+			continue;
 
+		putchar(',');
+		putchar(' ');
+	}
 
-if ((number_left == 56) && (number_right == 78))
-{
-break;
-}
+	}
 
-putchar(',');
-putchar (' ');
+	}
 
-}
-
-}
-
-putchar('\n');
-
-
-return (0);
-
+	putchar('\n')
+	
+	return (0)
 
 }
