eed2db183eac4cf2c08a40c2531b97d7c22f5064
okafordizzy <okaforchiedozie69@gmail.com>
2022-06-07 21:44:16 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -prints add two numbers
+ *@y: print int y
+ *@z: print int z
+ * Return: Always 0.
+ */
+
+int add(int y, int z)
+{
+return (y + z);
+}
