7ca44e6206be2a1d71b965bbcc7fe84f69188759
okafordizzy <okaforchiedozie69@gmail.com>
2022-06-07 21:51:49 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,22 +1,51 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_to_98 - print all natural numbers from n to 98.
- * @n: the number to start counting from n to 98
- * Return: Always 0.
+ * print_times_table - Prints a multiplication table up to param
+ * @n: The number to be treated
+ *
+ * Return: Number matrix
  */
-void print_to_98(int n)
+void print_times_table(int n)
 {
-if (n < 98)
-{
-for (n = n; n < 98; n++)
-printf("%d, ", n);
-printf("%d\n", 98);
-}
-else
-{
-for (n = n; n > 98; n--)
-printf("%d, ", n);
-printf("%d\n", 98);
-}
+	int x, y, z;
+
+	if (n >= 0 && n <= 14)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
